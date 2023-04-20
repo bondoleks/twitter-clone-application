be133f94b46dc1c691e501a87adbb44fafe8a3c5
Bondarchuk Oleksandr <110667393+bondoleks@users.noreply.github.com>
2023-04-20 14:43:07 +0300

    Dev/bondarchuk (#18)
    * Git CI
    
    * Checkstyle
    
    * SonarQube (1)
    
    * SonarQube (2)
    
    * SonarQube (3)
    
    * SonarQube (4)
    
    * SonarQube (5)
    
    * SonarQube (6)
    
    * SonarQube (7)
    
    * Coveralls
    
    * Coveralls (2)
    
    * Coveralls (3)
    
    * Coveralls (4)
    
    * Coveralls (5)
    
    * Coveralls (6)
    
    * Coveralls (7)
    
    * Coveralls (8)
    
    * Coveralls (9)
    
    * Coveralls (10)
    
    * Coveralls (11)
    
    * Coveralls (12)
    
    * Coveralls (12)

--- src/test/java/demo/project/twitter/TwitterApplicationTests.java
+++ src/test/java/demo/project/twitter/TwitterApplicationTests.java
@@ -10,30 +10,7 @@ class TwitterApplicationTests {
 
 	@Test
 	void contextLoads() {
-		User user = new User("Test");
-		Assert.isTrue(user.getName().equals("Test"));
-	@Test
-	void contextLoads2() {
-		User user = new User("Test2");
-		Assert.isTrue(user.getName().equals("Test2"));
+//		User user = new User("Test");
+//		Assert.isTrue(user.getName().equals("Test"));
 	}
-
-	@Test
-	void contextLoads3() {
-		User user = new User("Test3");
-		Assert.isTrue(user.getName().equals("Test3"));
-	}
-
-	@Test
-	void contextLoads2() {
-		User user = new User("Test2");
-		Assert.isTrue(user.getName().equals("Test2"));
-	}
-
-	@Test
-	void contextLoads3() {
-		User user = new User("Test3");
-		Assert.isTrue(user.getName().equals("Test3"));
-	}
-
 }
