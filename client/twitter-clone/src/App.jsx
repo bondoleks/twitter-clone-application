a207277a410d83824fe27ee423c4ceee00e5fbf5
Olena <37847084+OlenaBekhtir@users.noreply.github.com>
2023-04-22 23:32:21 +0300

    add routing and basic config pages for project (#21)
    * add folder client & config vite
    
    * change name of script dev to start in package.json
    
    * add routing and basic config pages for project
    
    ---------
    
    Co-authored-by: Olena Bekhtir <elena.behtir@gmail.com>

--- client/twitter-clone/src/App.jsx
+++ client/twitter-clone/src/App.jsx
@@ -1,15 +1,36 @@
-import { useState } from 'react'
+import { Link } from 'react-router-dom'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <div className='App'>
 
       <h1>Twitter-clone</h1>
 
-    </>
+      <nav>
+          <ul>
+            <li>
+              <Link to={`/home`}>Home</Link>
+            </li>
+            <li>
+              <Link to={`/explore`}>Explore</Link>
+            </li>
+            <li>
+              <Link to={`/notifications`}>Notifications</Link>
+            </li>
+            <li>
+              <Link to={`/messages`}>Messages</Link>
+            </li>
+            <li>
+              <Link to={`/user_profile`}>User_profile</Link>
+            </li>
+          </ul>
+        </nav>
+        
+    </div>
+
   )
 }
 
