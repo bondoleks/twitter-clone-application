dfd571e65c1b8c9f8a5e06a6351ef72f440959da
Alex Strebkov <113849226+Alex-Strebkov@users.noreply.github.com>
2023-04-24 12:32:33 +0300

    Added MUI, basic components and icons (#22)

--- client/twitter-clone/src/App.jsx
+++ client/twitter-clone/src/App.jsx
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom'
 import './App.css'
+import Button from '@mui/material/Button'
 
 
 function App() {
@@ -8,6 +9,10 @@ function App() {
     <div className='App'>
 
       <h1>Twitter-clone</h1>
+      <div>
+        <Button style={{margin: 10}} variant="contained">Hello World</Button>
+        <Button variant="outlined">Hello React</Button>
+      </div>
 
       <nav>
           <ul>
