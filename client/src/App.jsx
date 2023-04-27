cc1f4a3112a567091623de9c21df385793bf42ba
awkwardWinkyface <barannik.r.v@gmail.com>
2023-04-27 22:20:50 +0300

    Added footer to main page w/o logic usage

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { Grid } from '@mui/material';
 import Box from '@mui/material/Box';
 import Sidebar from './components/Sidebar/Sidebar'
+import Footerlogin from "./components/Footerlogin/Footerlogin.jsx";
 
 function App() {
 
@@ -8,6 +9,7 @@ function App() {
     <Grid container spacing={2} sx={{margin: "48px"}}>
       <Sidebar />
       <Box sx={{ fontSize: '28px', fontWeight: 600, fontFamily: 'Roboto' }}>Twitter-clone</Box>
+        <Footerlogin/>
     </Grid>
 
   )
