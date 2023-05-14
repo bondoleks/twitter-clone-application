43331ec88b97bb350a531c60892ced4af7c39a48
Bondarchuk_Oleksandr <sbv121121@gmail.com>
2023-05-14 19:04:07 +0300

    Change security

--- src/main/java/demo/project/twitter/config/SecurityConfig.java
+++ src/main/java/demo/project/twitter/config/SecurityConfig.java
@@ -39,7 +39,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers(LOGIN_ENDPOINT).permitAll()
                 .antMatchers(ADMIN_ENDPOINT).hasRole("ADMIN")
-                .anyRequest().authenticated()
+//                .anyRequest().authenticated()
+                .anyRequest().permitAll()
                 .and()
                 .apply(new JwtConfigurer(jwtTokenProvider));
     }
