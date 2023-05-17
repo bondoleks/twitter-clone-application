900149f460f8c2a2e4640258a85d07cf67aa1bac
Bondarchuk_Oleksandr <sbv121121@gmail.com>
2023-05-18 00:36:47 +0300

    Add first version WebSocket

---
+++ src/main/java/demo/project/twitter/webSocket/MessageOnlyTest.java
@@ -0,0 +1,15 @@
+package demo.project.twitter.webSocket;
+
+public class MessageOnlyTest {
+
+    private String from;
+    private String text;
+
+    public String getText() {
+        return text;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+}
