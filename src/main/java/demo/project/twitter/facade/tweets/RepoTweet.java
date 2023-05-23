75409963cd2eeb9bc82ed387ab7ac91011c8650d
Bondarchuk Oleksandr <110667393+bondoleks@users.noreply.github.com>
2023-05-23 11:26:55 +0300

    Add notification endpoints (#65)

--- src/main/java/demo/project/twitter/facade/tweets/RepoTweet.java
+++ src/main/java/demo/project/twitter/facade/tweets/RepoTweet.java
@@ -1,9 +1,10 @@
 package demo.project.twitter.facade.tweets;
 
 import demo.project.twitter.model.tweet.Tweet;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface RepoTweet extends CrudRepository<Tweet, Long> { }
+public interface RepoTweet extends JpaRepository<Tweet, Long> { }
--- src/main/java/demo/project/twitter/facade/users/RepoUser.java
+++ src/main/java/demo/project/twitter/facade/users/RepoUser.java
@@ -1,9 +1,12 @@
 package demo.project.twitter.facade.users;
 
 import demo.project.twitter.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface RepoUser extends CrudRepository<User, Long> { }
+public interface RepoUser extends JpaRepository<User, Long> {
+    Long getUserIdByUsername(String username);
+
+}
