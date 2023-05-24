f0feaf46f383360b55dbc8ab2723fad22dbb3b38
ArtemPylypenko <numb432hz@gmail.com>
2023-05-24 16:16:47 +0300

    messages endpoint

--- src/main/java/demo/project/twitter/facade/masseges/RepoMessage.java
+++ src/main/java/demo/project/twitter/facade/masseges/RepoMessage.java
@@ -1,9 +1,11 @@
 package demo.project.twitter.facade.masseges;
 
 import demo.project.twitter.model.chat.Message;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface RepoMessage extends CrudRepository<Message, Long> { }
+public interface RepoMessage extends JpaRepository<Message, Long> {
+
+}
