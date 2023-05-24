c14f0a162a1b29fd8ab13ae370f82302a786e828
Bondarchuk Oleksandr <110667393+bondoleks@users.noreply.github.com>
2023-05-24 12:16:08 +0300

    Notification endpoints (#68)

--- src/main/java/demo/project/twitter/facade/notifications/ControllerNotification.java
+++ src/main/java/demo/project/twitter/facade/notifications/ControllerNotification.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RequiredArgsConstructor
 @RestController
 
@@ -15,31 +17,25 @@ import org.springframework.web.bind.annotation.*;
 public class ControllerNotification {
 
     private final ServiceNotification serviceNotification;
-    private final
 
 
-    @PostMapping("create/{fromUserId}/{tweetId}")
-    String createNotification(@PathVariable("fromUserId") Long fromUserId, @PathVariable("tweetId") Long tweetId,
-                          @RequestParam NotificationType notificationType, @RequestParam String toUsername, Model model){
-        serviceNotification.createNotification(notificationType, toUsername, fromUserId, tweetId);
-        return "Created";
-//        return "redirect:/ name attribute in front";
-    }
+//    @PostMapping("create/{fromUserId}/{tweetId}")
+//    String createNotification(@PathVariable("fromUserId") Long fromUserId, @PathVariable("tweetId") Long tweetId,
+//                          @RequestParam NotificationType notificationType, @RequestParam String toUsername, Model model){
+//        serviceNotification.createNotification(notificationType, toUsername, fromUserId, tweetId);
+//        return "Created";
+////        return "redirect:/ name attribute in front";
+//    }
 
-    @GetMapping("read/{id}")
-    public String readNotificationsByUserId(@PathVariable("id") Long id, Model model) {
-        Iterable<Notification> posts = serviceNotification.findAllNotificationByRecieverId(id);
-        model.addAttribute("name attribute in front", posts);
-        return posts.toString();
-//        return "name attribute in front";
+    @PostMapping("read/{id}")
+    public List<Notification> readNotificationsByUserId(@PathVariable("id") Long id) {
+        return serviceNotification.findAllNotificationByRecieverId(id);
     }
 
     @PostMapping("{id}/delete")
-    public String deleteNotification(@PathVariable(value = "id") Long id, Model model){
+    public List<Notification> deleteNotification(@PathVariable(value = "id") Long id, Model model){
         serviceNotification.deleteNotification(id);
-        return "Deleted";
-//        return "redirect:/ name attribute in front";
-
+        return serviceNotification.findAllNotificationByRecieverId(id);
     }
 
 }
