ce102fd3deed9105902049d078155b4b0c4d1751
Bondarchuk Oleksandr <110667393+bondoleks@users.noreply.github.com>
2023-05-25 23:39:02 +0300

    Notification endpoints (#76)
    * Add notification endpoints
    
    * Fix notification endpoints
    
    * Fix security
    
    * Fix security 2

--- src/main/java/demo/project/twitter/config/SecurityConfig.java
+++ src/main/java/demo/project/twitter/config/SecurityConfig.java
@@ -9,6 +9,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -43,5 +44,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //                .anyRequest().permitAll()
                 .and()
                 .apply(new JwtConfigurer(jwtTokenProvider));
+        http.cors().configurationSource(rq -> new CorsConfiguration().applyPermitDefaultValues());
     }
 }
