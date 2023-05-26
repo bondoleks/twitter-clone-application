d0e5e6b73a8591cb3d025b2affda55d2527ec8f5
Alex Strebkov <113849226+Alex-Strebkov@users.noreply.github.com>
2023-05-27 01:41:28 +0300

    added messages page (#77)

---
+++ client/src/pages/Messages/Components/MessagesRightColumn.jsx
@@ -0,0 +1,57 @@
+import { Box, Button, Typography } from "@mui/material";
+import React, { useState } from "react";
+import NewMessageModal from "./NewMessageModal.jsx";
+
+const MessagesRightColumn = () => {
+  const [modalOpen, setModalOpen] = useState(false);
+
+  const openModal = () => {
+    setModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        height: "100%",
+        margin: "0 50px",
+      }}
+    >
+      <Typography
+        sx={{
+          fontSize: "30px",
+          fontWeight: "800",
+          paddingBottom: "10px",
+        }}
+      >
+        Select a message
+      </Typography>
+      <Typography sx={{ paddingBottom: "10px" }}>
+        Choose from your existing conversations, start a new one, or just keep swimming.
+      </Typography>
+
+      <Button
+        variant="contained"
+        onClick={openModal}
+        sx={{
+          borderRadius: "20px",
+          marginTop: "20px",
+          width: "200px",
+          backgroundColor: "rgb(29, 155, 240)",
+        }}
+      >
+        New Message
+      </Button>
+
+      <NewMessageModal open={modalOpen} closeModal={closeModal} />
+    </Box>
+  );
+};
+
+export default MessagesRightColumn;
---
+++ client/src/pages/Messages/Components/NewMessageModal.jsx
@@ -0,0 +1,85 @@
+import {Box, Button, Dialog, IconButton, InputAdornment, Paper, TextField, Typography, useTheme} from "@mui/material";
+import CloseIcon from "@mui/icons-material/Close";
+import React, { useState } from "react";
+import {styled} from "@mui/system";
+import SearchIcon from "@mui/icons-material/Search.js";
+import Groups2RoundedIcon from "@mui/icons-material/Groups2Rounded";
+import ModalList from "./ModalList.jsx";
+import useMediaQuery from '@mui/material/useMediaQuery';
+
+
+const StyledSearchIcon = styled(SearchIcon)(({ inputFocus }) => ({
+  color: inputFocus ? "rgb(29, 155, 240)" : "grey",
+}));
+
+const NewMessageModal = ({ open, closeModal }) => {
+  const [inputFocus, setInputFocus] = useState(false);
+
+  const handleInputFocus = () => {
+    setInputFocus(true);
+  };
+
+  const handleInputBlur = () => {
+    setInputFocus(false);
+  };
+
+  const theme = useTheme();
+  const fullScreen = useMediaQuery(theme.breakpoints.down('md'));
+
+  return (
+    <Dialog
+     open={open}
+      onClose={closeModal}
+     fullScreen={fullScreen}
+     PaperProps={{
+       sx: {
+         borderRadius: "15px"
+
+       },
+     }}>
+      <Paper sx={{ padding: "20px", width: "550px",
+        height: "500px" }}>
+        <Box sx={{ display: "flex", justifyContent: "space-between" }}>
+          <IconButton edge="start" onClick={closeModal}>
+            <CloseIcon />
+          </IconButton>
+          <Typography sx={{ fontSize: "26px", fontWeight: "800" }}>New Message</Typography>
+          <Button variant="contained" disabled sx={{ borderRadius: "20px", color: "black" }}>
+            Next
+          </Button>
+        </Box>
+        <TextField
+          id="standard-basic"
+          placeholder="Search People"
+          variant="standard"
+          onFocus={handleInputFocus}
+          onBlur={handleInputBlur}
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <StyledSearchIcon inputFocus={inputFocus} />
+              </InputAdornment>
+            ),
+          }}
+          sx={{
+            width: "100%",
+          }}
+        />
+        <Box sx={{ display: "flex", alignItems: "center", gap: "18px", marginTop: "20px" }}>
+          <IconButton>
+            <Groups2RoundedIcon sx={{ color: "rgb(29, 155, 240)" }} />
+          </IconButton>
+          <Typography sx={{ fontSize: "20px", fontWeight: "600", color: "rgb(29, 155, 240)" }}>
+            Create Group
+          </Typography>
+        </Box>
+        <Box>
+          <ModalList />
+        </Box>
+      </Paper>
+    </Dialog>
+  );
+};
+
+export default NewMessageModal;
+
