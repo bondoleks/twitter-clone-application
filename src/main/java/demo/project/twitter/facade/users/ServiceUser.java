3487e476f26f3d9494ff0620fe9f70659e7d7a9a
S2909 <118194722+S2909@users.noreply.github.com>
2023-05-29 12:16:54 +0300

    usr endpoints (#84)
    * usr endpoints
    
    * usr endpoints2

--- src/main/java/demo/project/twitter/facade/users/ServiceUser.java
+++ src/main/java/demo/project/twitter/facade/users/ServiceUser.java
@@ -1,38 +1,90 @@
 package demo.project.twitter.facade.users;
 
 
+import demo.project.twitter.dto.UserDto;
 import demo.project.twitter.model.User;
 
+import demo.project.twitter.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
 
 import java.util.Optional;
+import java.util.Set;
 
-@org.springframework.stereotype.Service
+@Service
+@Slf4j
 @RequiredArgsConstructor
 public class ServiceUser implements FunctionUser {
-    private final RepoUser repo;
+    private final UserRepository repo;
 
-   /* Дальнейший код приведен для примера.
-        В данном классе создаются методы сервиса, заявленные в интерфейсе Function,
-        в том числе, и с подключением интерфейса Repo (наследник CrudRepository)
-        */
-
-// ************************************** EXAMPLE START **************************************
-
-    @Override
-    public User saveOne(User user) {
-        return repo.save(user);
-    }
-    @Override
-    public Optional<User> getById(Long id) {
-        return repo.findById(id);
+    public Boolean user_exists(User usr) {
+        if (repo.getUserIdByUsername(usr.getUsername()) != null) return true;
+        else return false;
     }
 
     @Override
-    public boolean existsById(Long id) {
-        return repo.existsById(id);
+    public User saveUser(User user) {
+        repo.save(user);
+        return user;
     }
 
-//    ************************************** EXAMPLE END **************************************
+    @Override
+    public User updateUser(User usr, UserDto data) {
 
+        if (user_exists(usr)) {
+
+            usr.setUsername(Optional.ofNullable(data.getUsername()).orElse(usr.getUsername()));
+            usr.setFirstName(Optional.ofNullable(data.getFirstName()).orElse(usr.getFirstName()));
+            usr.setLastName(Optional.ofNullable(data.getLastName()).orElse(usr.getLastName()));
+            usr.setLocation(Optional.ofNullable(data.getLocation()).orElse(usr.getLocation()));
+            usr.setBirthDate(Optional.ofNullable(data.getBirthdate()).orElse(usr.getBirthDate()));
+            usr.setBio(Optional.ofNullable(data.getBio()).orElse(usr.getBio()));
+            usr.setEmail(Optional.ofNullable(data.getEmail()).orElse(usr.getEmail()));
+            usr.setAv_imagerUrl(Optional.ofNullable(data.getAv_imagerUrl()).orElse(usr.getAv_imagerUrl()));
+            usr.setHead_imagerUrl(Optional.ofNullable(data.getHead_imagerUrl()).orElse(usr.getHead_imagerUrl()));
+            return usr;
+        } else return null;
+    }
+
+
+    @Override
+    public User findById(Long id) {
+        User usr = repo.findById(id).orElse(null);
+
+        if (usr == null) {
+            log.warn("IN findById - no user found by id: {}", id);
+            return null;
+        }
+
+        return usr;
+    }
+
+
+    @Override
+    public Set<User> getFollowers(User user) {
+
+        Optional<User> usr = repo.findById(user.getId());
+        if (usr.isEmpty()) {
+            return null;
+        } else
+            return usr.get().getFollowers();
+    }
+
+
+
+
+
+    @Override
+    public Set<User> getFollowing(User user) {
+        Optional<User> usr = repo.findById(user.getId());
+        if (usr.isEmpty()){
+            return null;
+        }
+        else
+            return usr.get().getFollowings();
+    }
 }
+
+
