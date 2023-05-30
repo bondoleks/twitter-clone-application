540be6621e0668dabcd1f215a6d99be576c29ebb
Alex <alexstrwork19@gmail.com>
2023-05-30 10:18:26 +0300

    fixed the messages page

--- client/src/pages/Messages/Messages.jsx
+++ client/src/pages/Messages/Messages.jsx
@@ -9,10 +9,12 @@ const Messages = () => {
 
   return (
     <Grid container spacing={3} sx={{ height: '100vh', marginLeft: '24px' }}>
+      <Hidden smDown>
       <Grid item xs={3} md={3} sm={3} lg={3}>
         <Sidebar />
       </Grid>
-      <Grid item xs={9} md={9} sm={9} lg={4} sx={{ borderRight: '1px solid grey', borderLeft: '1px solid grey'}}>
+      </Hidden>
+      <Grid item xs={12} md={9} sm={9} lg={4} sx={{ borderRight: '1px solid rgba(128, 128, 128, 0.1)', borderLeft: '1px solid rgba(128, 128, 128, 0.1)'}}>
         <MessageMiddleColumn />
       </Grid>
       <Hidden smDown>
