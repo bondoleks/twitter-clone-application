492d3ca9b8488ec4c3cecf79e079f1575c1e1de1
Ortemka <numb432hz@gmail.com>
2023-06-09 14:18:04 +0300

    Update ControllerChat.java

--- src/main/java/demo/project/twitter/facade/chats/ControllerChat.java
+++ src/main/java/demo/project/twitter/facade/chats/ControllerChat.java
@@ -22,14 +22,5 @@ public class ControllerChat {
     public ResponseEntity<?> saveEntity(@RequestBody DtoChatReq dto) {
         return facade.saveEntity(dto);
     }
-
-    @GetMapping("get/{userToId}")
-    public ResponseEntity<?> getEntity(@PathVariable("userToId") Long id) {
-        return facade.getEntity(id);
-    }
-    @GetMapping("getChat")
-    public ResponseEntity<?> getChatBetweenUsers(@RequestBody() DtoChatReq dtoReq) {
-        return facade.getChatBetweenUsers(dtoReq);
-    }
-
+    
 }
