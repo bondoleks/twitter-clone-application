867f368b56609534efe4c2bd4da79e79209c3327
Bondarchuk Oleksandr <110667393+bondoleks@users.noreply.github.com>
2023-06-13 13:05:29 +0300

    Registration (#127)
    * RegistrationController
    
    * Fix remarks after the meeting
    
    * Fix user
    
    * Add Mail Activated
    
    * Add Mail Activated (post, boolean)
    
    * Fix endpoints
    
    * Fix security
    
    * Cors

--- src/main/java/demo/project/twitter/config/SecurityConfig.java
+++ src/main/java/demo/project/twitter/config/SecurityConfig.java
@@ -50,10 +50,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .antMatchers(LOGIN_ENDPOINT).permitAll()
                 .antMatchers(ADMIN_ENDPOINT).hasRole("ADMIN")
-                .antMatchers("/", "/api/v1/auth/registration", "/activate/*", "/login", "/registration").permitAll()
+                .antMatchers("/", "/api/v1/auth/registration", "/api/v1/auth/activate/*", "/login", "/registration").permitAll()
                 .anyRequest().authenticated().and()
                 .apply(new JwtConfigurer(jwtTokenProvider));
-//        http.cors().configurationSource(rq -> new CorsConfiguration().applyPermitDefaultValues());
+        http.cors().configurationSource(rq -> new CorsConfiguration().applyPermitDefaultValues());
     }
 
 //    @Bean
