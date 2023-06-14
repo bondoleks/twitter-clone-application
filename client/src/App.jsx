a912df11dbceffaa34d3be712a4b5ef9a463de4a
Olena Bekhtir <elena.behtir@gmail.com>
2023-06-14 21:04:11 +0300

    fix but tweet on homepage & add routing in profile page for following&followers

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -19,7 +19,6 @@ import { MessagesContextProvider } from './context/messagesContext.jsx';
 import ActiveChat from './pages/Messages/Components/ActiveChat.jsx';
 import ProfileId from './pages/Profile/ProfileId';
 import ProfileUser from './pages/Profile/ProfileUser';
-
 import ProfileFollowers from './pages/ProfileFollowers/ProfileFollowers';
 import ProfileFollowing from './pages/ProfileFollowing/ProfileFollowing';
 
@@ -91,15 +90,9 @@ const routes = [
 
         // element: <ProfileUser />,
         element: <PrivateRoute element={ProfileUser} />,
-
-        children: <>
-            <Route path={'followers'} element={<ProfileFollowers />} />
-            <Route path={'following'} element={<ProfileFollowing />} />
-        </>
-
     },
     {
-        path: "/profile:id",
+        path: "/profile/:id",
         // element: <ProfileId />,
         element: <PrivateRoute element={ProfileId} />,
 
