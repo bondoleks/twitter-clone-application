b5ce775e287db2d5f0520cf200a2cab68235f357
S2909 <118194722+S2909@users.noreply.github.com>
2023-06-16 10:20:19 +0300

    User endpoints (#140)
    * usr endpoints
    
    * usr endpoints2
    
    * usr endpoints3
    
    * usr endpoints4
    
    * endpt5
    
    * endpt6
    
    * endpt7
    
    * endpt9
    
    * endpt10 imports back
    
    * Update SecurityConfig.java

--- src/main/java/demo/project/twitter/config/SecurityConfig.java
+++ src/main/java/demo/project/twitter/config/SecurityConfig.java
@@ -13,6 +13,8 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.cors.CorsConfiguration
+
 
 
 @Configuration
