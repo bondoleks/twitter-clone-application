7d1b0bf5e7c5eecf248ede8c5fd86135426fdcbf
Sergiy Semenko <ssmnko2@gmail.com>
2023-06-16 12:12:47 +0200

    SOS 0616

--- src/main/java/demo/project/twitter/config/SecurityConfig.java
+++ src/main/java/demo/project/twitter/config/SecurityConfig.java
@@ -13,7 +13,7 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfiguration;
 
 
 
