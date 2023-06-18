b4e661afd6de3ffd3edd5ef4c5dd0344251c9a5d
Olena Bekhtir <elena.behtir@gmail.com>
2023-06-18 21:37:03 +0300

    fix autorization for email

--- client/src/hooks/UseFetch.js
+++ client/src/hooks/UseFetch.js
@@ -1,14 +1,14 @@
 import { useCallback, useEffect, useState } from 'react';
-import { api } from "../redux/service/api";
+import api from 'axios';
 
 export const useFetch = ({
-    instant = true,
-    initData = null,
-    onCompleted = (data) => data,
-    dataTransformer = (data) => data,
-    onError = (err) => err,
-    ...axios
-}) => {
+                             instant = true,
+                             initData = null,
+                             onCompleted = (data) => data,
+                             dataTransformer = (data) => data,
+                             onError = (err) => err,
+                             ...axios
+                         }) => {
     const [data, setData] = useState(initData);
     const [loading, setLoading] = useState(false);
 
@@ -23,22 +23,7 @@ export const useFetch = ({
 
             setLoading(true);
 
-            const formData = new FormData(); // Create a FormData object
-
-            // Add the request data to the FormData object
-            for (const key in fetchOptions.data) {
-                formData.append(key, fetchOptions.data[key]);
-            }
-
-            fetchOptions.headers = {
-                ...fetchOptions.headers,
-                'Content-Type': 'multipart/form-data' // Set the Content-Type header
-            };
-
-            return api({
-                ...fetchOptions,
-                data: formData // Use the FormData object as the request data
-            })
+            return api(fetchOptions)
                 .then(dataTransformer)
                 .then((res) => {
                     setData(res);
@@ -50,6 +35,7 @@ export const useFetch = ({
                     setLoading(false);
                 });
         },
+        // eslint-disable-next-line
         [onCompleted, dataTransformer]
     );
 
@@ -57,6 +43,7 @@ export const useFetch = ({
         if (instant) {
             fetcher(axios);
         }
+        // eslint-disable-next-line
     }, []);
 
     return [
@@ -68,3 +55,75 @@ export const useFetch = ({
         fetcher
     ];
 };
+
+
+// import { useCallback, useEffect, useState } from 'react';
+// import { api } from "../redux/service/api";
+
+// export const useFetch = ({
+//     instant = true,
+//     initData = null,
+//     onCompleted = (data) => data,
+//     dataTransformer = (data) => data,
+//     onError = (err) => err,
+//     ...axios
+// }) => {
+//     const [data, setData] = useState(initData);
+//     const [loading, setLoading] = useState(false);
+
+//     const fetcher = useCallback(
+//         (options) => {
+//             const fetchOptions = { ...axios, ...options };
+//             if (fetchOptions.paramsStringify) {
+//                 fetchOptions.url = `${fetchOptions.url}?${JSON.stringify(
+//                     fetchOptions.paramsStringify
+//                 )}`;
+//             }
+
+//             setLoading(true);
+
+//             const formData = new FormData(); // Create a FormData object
+
+//             // Add the request data to the FormData object
+//             for (const key in fetchOptions.data) {
+//                 formData.append(key, fetchOptions.data[key]);
+//             }
+
+//             fetchOptions.headers = {
+//                 ...fetchOptions.headers,
+//                 'Content-Type': 'multipart/form-data' // Set the Content-Type header
+//             };
+
+//             return api({
+//                 ...fetchOptions,
+//                 data: formData // Use the FormData object as the request data
+//             })
+//                 .then(dataTransformer)
+//                 .then((res) => {
+//                     setData(res);
+//                     onCompleted(res, options);
+//                     return res;
+//                 })
+//                 .catch(onError)
+//                 .finally(() => {
+//                     setLoading(false);
+//                 });
+//         },
+//         [onCompleted, dataTransformer]
+//     );
+
+//     useEffect(() => {
+//         if (instant) {
+//             fetcher(axios);
+//         }
+//     }, []);
+
+//     return [
+//         {
+//             data,
+//             loading,
+//             modifyData: setData
+//         },
+//         fetcher
+//     ];
+// };
