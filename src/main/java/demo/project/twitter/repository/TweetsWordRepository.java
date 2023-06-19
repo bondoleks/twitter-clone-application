6fdfc2d6fe19c377afba7a446debdf00491bcaf1
Ssmnko2 <113474715+Ssmnko2@users.noreply.github.com>
2023-06-19 09:47:02 +0300

    sos0618 (#149)

--- src/main/java/demo/project/twitter/repository/TweetsWordRepository.java
+++
@@ -1,7 +0,0 @@
-package demo.project.twitter.repository;
-
-import demo.project.twitter.model.TweetsWord;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TweetsWordRepository extends JpaRepository<TweetsWord, Long> {
-}
