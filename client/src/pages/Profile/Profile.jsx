b33e927801d58fd5403e52c6846f8528448bc4ef
Olena <37847084+OlenaBekhtir@users.noreply.github.com>
2023-06-26 18:56:49 +0300

    Olena modaledituser (#194)
    * fixed padding& theme color in some modals
    
    * fixed private routing
    
    * change modal  edituser
    
    ---------
    
    Co-authored-by: Olena Bekhtir <elena.behtir@gmail.com>

--- client/src/pages/Profile/Profile.jsx
+++ client/src/pages/Profile/Profile.jsx
@@ -73,9 +73,7 @@ export const Profile = ({ withId }) => {
             width: '110%',
             marginLeft: '-5%',
             height: '200px',
-          }}></Box>
-
-          
+          }}></Box>       
 
 
           <StyledAvatar
