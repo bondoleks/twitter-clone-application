625fd5fd2e564835bf7a81a9f44455e7366cad42
Bondarchuk_Oleksandr <sbv121121@gmail.com>
2023-07-04 17:29:59 +0300

    Fix token oauth, and readme

--- src/main/java/demo/project/twitter/security/oauth/OAuth2LoginSuccessHandler.java
+++ src/main/java/demo/project/twitter/security/oauth/OAuth2LoginSuccessHandler.java
@@ -1,8 +1,10 @@
 package demo.project.twitter.security.oauth;
 
 import demo.project.twitter.model.User;
+import demo.project.twitter.security.jwt.JwtTokenProvider;
 import demo.project.twitter.service.impl.UserServiceImpl;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
@@ -15,6 +17,10 @@ import java.io.IOException;
 @Component
 @Log4j2
 public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private AuthenticationManager authenticationManager;
+
+    private JwtTokenProvider jwtTokenProvider;
 
     private UserServiceImpl userService;
 
@@ -34,8 +40,10 @@ public class OAuth2LoginSuccessHandler extends SimpleUrlAuthenticationSuccessHan
         if(userFromGoogle == null){
             User newUserFromGoogle = new User(username, email);
             userService.registerFromGoogle(newUserFromGoogle, name);
+            String token = jwtTokenProvider.createToken(username, newUserFromGoogle.getRoles());
         }else {
             userService.updateUserAfterGoogleLogin(userFromGoogle, email);
+            String token = jwtTokenProvider.createToken(username, userFromGoogle.getRoles());
         }
 
         super.onAuthenticationSuccess(request, response, authentication);
